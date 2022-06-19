2264b6c1d2defa185c26e18a367d07092ed2eca1
Paulchch <83615379+Paulchch@users.noreply.github.com>
2022-06-19 14:55:37 -0600

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include "IFoodFactory.h"
+#include "Factory.h"
 
 int main(){
-    std::cout<<"Hello"<<std::endl;
-    return 0;
+    const double TAX = 0.13; //Our tax will be 13%
+    std::cout<<"Welcome to our restaurant"<<std::endl;
+
+    IFoodFactory* foodFactory = new Factory(); //The factory
+
+    IFood* burger;
+    IFood* taco;
+
+    burger = foodFactory->createFood("BURGER");
+    taco = foodFactory->createFood("TACO");
+
+    std::cout<<"Burger total price is: "<<burger->totalPrice(TAX)<<" dollars"<<std::endl;
+    std::cout<<"Taco total price is: "<<taco->totalPrice(TAX)<<" dollars"<<std::endl;
+
 }
